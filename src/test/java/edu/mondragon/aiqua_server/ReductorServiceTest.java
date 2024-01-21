a49c2c250ab0cfebf7a79ef52914e6a95ab67384
Nagore <nagore.elduaien@alumni.mondragon.edu>
2024-01-21 14:35:58 +0100

    back

--- src/test/java/edu/mondragon/aiqua_server/ReductorServiceTest.java
+++ src/test/java/edu/mondragon/aiqua_server/ReductorServiceTest.java
@@ -1,6 +1,5 @@
 package edu.mondragon.aiqua_server;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
--- src/test/java/edu/mondragon/aiqua_server/TownServiceTest.java
+++ src/test/java/edu/mondragon/aiqua_server/TownServiceTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
